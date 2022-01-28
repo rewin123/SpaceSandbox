632de66a6f11503493cd8ed7dfd793851bb2254a
a.yamaev@smartengines.com <a.yamaev@smartengines.com>
2022-01-28 22:55:05 +0300

    Move camera

--- engine/src/lib.rs
+++ engine/src/lib.rs
@@ -4,7 +4,28 @@ pub mod wavefront;
 pub mod mesh;
 pub mod gpu;
 pub mod loop_game;
+pub mod camera;
 
-// pub struct Engine {
 
-// }
+use crate::{resource::*};
+use crate::gpu::GPU;
+use winit::window::Window;
+
+pub struct Engine {
+    gpu : GPU, 
+    file_res_system : FileResourceEngine
+}
+
+impl Engine {
+    pub async fn from_window(window : &Window, res_path : &String) -> Self {
+        let gpu = gpu::GPU::from_window(window).await;
+
+        let mut file_res_system = FileResourceEngine::default();
+        file_res_system.init(res_path);
+
+        Self {
+            gpu,
+            file_res_system
+        }
+    }
+}
