e416da0906a459e3648d841c2da02c63d6114af1
Andrey Yamaev <a.yamaev@smartengines.com>
2022-03-03 19:57:31 +0300

    ENH: benchmark for wavefront loading

--- src/mesh/mod.rs
+++ src/mesh/mod.rs
@@ -13,6 +13,8 @@ vulkano::impl_vertex!(Vertex, position);
 #[derive(Debug)]
 pub struct CpuMesh {
     pub poses : Vec<Vec3>,
+    pub normals : Vec<Vec3>,
+    pub uvs : Vec<Vec2>,
     pub indices : Vec<u32>
 }
 
@@ -28,17 +30,34 @@ fn wavefront_indices(data : &str) -> (i32, i32, i32) {
      split[2].parse().unwrap())
 }
 
+fn get_vec3(words : &Vec<String>) -> Vec3 {
+    Vec3::new(
+        words[1].parse().unwrap(),
+        words[2].parse().unwrap(),
+        words[3].parse().unwrap())
+}
+
+fn get_vec2(words : &Vec<String>) -> Vec2 {
+    Vec2 {
+        data : [
+            words[1].parse().unwrap(),
+            words[2].parse().unwrap()]
+    }
+}
+
 pub fn wavefront_mesh_from_file(path : String) -> Option<Arc<CpuMesh>> {
 
     let file = File::open(path).unwrap();
     let reader = BufReader::new(file);
 
     let mut poses = vec![];
-    // let mut normals = vec![];
-    // let mut uvs = vec![];
-    let mut idx_shift = 0;
+    let mut normals = vec![];
+    let mut uvs = vec![];
+    let mut idx_shift : i32 = 0;
 
     let mut vertex_poses = vec![];
+    let mut vertex_normals = vec![];
+    let mut vertex_uvs = vec![];
     let mut indices : Vec<u32> = vec![];
 
     let mut idx_map = HashMap::new();
@@ -57,19 +76,25 @@ pub fn wavefront_mesh_from_file(path : String) -> Option<Arc<CpuMesh>> {
 
         if words[0] == "o" {
             //new object
-            idx_shift = poses.len();
+            idx_shift = poses.len() as i32;
+            idx_map.clear();
+            poses.clear();
+            normals.clear();
+            uvs.clear();
         } else if words[0] == "v" {
             //new pos
-            poses.push(Vec3::new(
-                words[1].parse().unwrap(), 
-                words[2].parse().unwrap(), 
-                words[3].parse().unwrap()));
+            poses.push(get_vec3(&words));
+        } else if words[0] == "vn" {
+            normals.push(get_vec3(&words));
+        } else if words[0] == "vt" {
+            uvs.push(get_vec2(&words));
         } else if words[0] == "f" {
             for i in 1..4 {
                 if !idx_map.contains_key(&words[i]) {
-                    let (pos_idx, norm_idx, uv_idx) = wavefront_indices(&words[i]);
+                    let (pos_idx, uv_idx, norm_idx) = wavefront_indices(&words[i]);
                     vertex_poses.push(poses[(pos_idx - 1) as usize]);
-
+                    vertex_normals.push(normals[(norm_idx - 1) as usize]);
+                    vertex_uvs.push(uvs[(uv_idx - 1) as usize]);
                     idx_map.insert(
                         words[i].clone(),
                         vertex_poses.len() - 1
@@ -78,11 +103,12 @@ pub fn wavefront_mesh_from_file(path : String) -> Option<Arc<CpuMesh>> {
                 indices.push(idx_map[&words[i]] as u32);
             }
         }
-
     }
 
     let mesh = CpuMesh {
         poses : vertex_poses,
+        normals : vertex_normals,
+        uvs : vertex_uvs,
         indices
     };
 
