37018f1e022018682726b3dd2272508320a5bbd8
Andrey Yamaev <a.yamaev@smartengines.com>
2022-09-13 15:09:10 +0300

    added methods new and fill for BufferSafe

--- src/lib.rs
+++ src/lib.rs
@@ -349,5 +349,62 @@ pub fn fill_commandbuffers(
 }
 
 pub struct BufferSafe {
-    pub buffer : vk::Buffer
+    pub buffer : vk::Buffer,
+    pub allocation : vk_mem::Allocation,
+    pub allocation_info : vk_mem::AllocationInfo,
+    pub allocator : Arc<AllocatorSafe>
+}
+
+impl BufferSafe {
+    pub fn new(
+        allocator: &Arc<AllocatorSafe>,
+        size_in_bytes: u64,
+        usage: vk::BufferUsageFlags,
+        memory_usage: vk_mem::MemoryUsage,
+    ) -> Result<BufferSafe, vk_mem::error::Error> {
+        let allocation_create_info = vk_mem::AllocationCreateInfo {
+            usage: memory_usage,
+            ..Default::default()
+        };
+        let (buffer, allocation, allocation_info) = allocator.create_buffer(
+            &ash::vk::BufferCreateInfo::builder()
+                .size(size_in_bytes)
+                .usage(usage)
+                .build(),
+            &allocation_create_info,
+        )?;
+        Ok(BufferSafe {
+            buffer,
+            allocation,
+            allocation_info,
+            allocator : allocator.clone()
+        })
+    }
+
+    pub fn fill<T: Sized>(
+        &self,
+        data: &[T],
+    ) -> Result<(), vk_mem::error::Error> {
+        let data_ptr = self.allocator.map_memory(&self.allocation)? as *mut T;
+        unsafe { data_ptr.copy_from_nonoverlapping(data.as_ptr(), data.len()) };
+        self.allocator.unmap_memory(&self.allocation);
+        Ok(())
+    }
+}
+
+impl Drop for BufferSafe {
+    fn drop(&mut self) {
+        info!("Destroy buffer");
+        unsafe {
+            self.allocator.destroy_buffer(self.buffer, &self.allocation);
+        }
+    }
+}
+
+impl Deref for BufferSafe {
+    type Target = vk::Buffer;
+
+    fn deref(&self) -> &Self::Target {
+        &self.buffer
+    }
 }
