eb2cbe2ae6b58858f5fb59df595ec06b504b7756
Andrey Yamaev <a.yamaev@smartengines.com>
2022-09-13 15:14:00 +0300

    move BufferSafe to standalone file

--- src/lib.rs
+++ src/lib.rs
@@ -21,6 +21,7 @@ pub mod instance_safe;
 pub mod debug_layer;
 pub mod vulkan_init_utils;
 pub mod example_pipeline;
+pub mod buffer_safe;
 
 pub use swapchain_safe::*;
 pub use surface_safe::*;
@@ -28,6 +29,7 @@ pub use instance_safe::*;
 pub use debug_layer::*;
 pub use vulkan_init_utils::*;
 use example_pipeline::*;
+pub use buffer_safe::*;
 
 pub struct GraphicBase {
     pub instance : Arc<InstanceSafe>,
@@ -346,65 +348,4 @@ pub fn fill_commandbuffers(
         }
     }
     Ok(())
-}
-
-pub struct BufferSafe {
-    pub buffer : vk::Buffer,
-    pub allocation : vk_mem::Allocation,
-    pub allocation_info : vk_mem::AllocationInfo,
-    pub allocator : Arc<AllocatorSafe>
-}
-
-impl BufferSafe {
-    pub fn new(
-        allocator: &Arc<AllocatorSafe>,
-        size_in_bytes: u64,
-        usage: vk::BufferUsageFlags,
-        memory_usage: vk_mem::MemoryUsage,
-    ) -> Result<BufferSafe, vk_mem::error::Error> {
-        let allocation_create_info = vk_mem::AllocationCreateInfo {
-            usage: memory_usage,
-            ..Default::default()
-        };
-        let (buffer, allocation, allocation_info) = allocator.create_buffer(
-            &ash::vk::BufferCreateInfo::builder()
-                .size(size_in_bytes)
-                .usage(usage)
-                .build(),
-            &allocation_create_info,
-        )?;
-        Ok(BufferSafe {
-            buffer,
-            allocation,
-            allocation_info,
-            allocator : allocator.clone()
-        })
-    }
-
-    pub fn fill<T: Sized>(
-        &self,
-        data: &[T],
-    ) -> Result<(), vk_mem::error::Error> {
-        let data_ptr = self.allocator.map_memory(&self.allocation)? as *mut T;
-        unsafe { data_ptr.copy_from_nonoverlapping(data.as_ptr(), data.len()) };
-        self.allocator.unmap_memory(&self.allocation);
-        Ok(())
-    }
-}
-
-impl Drop for BufferSafe {
-    fn drop(&mut self) {
-        info!("Destroy buffer");
-        unsafe {
-            self.allocator.destroy_buffer(self.buffer, &self.allocation);
-        }
-    }
-}
-
-impl Deref for BufferSafe {
-    type Target = vk::Buffer;
-
-    fn deref(&self) -> &Self::Target {
-        &self.buffer
-    }
 }
