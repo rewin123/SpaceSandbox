6c8c09efe490be17010d77aacbe12d6f5c86f86a
Andrey Yamaev <a.yamaev@smartengines.com>
2022-11-15 17:54:30 +0300

    add: spawning target entity

--- crates/space_assets/src/gltf_loader.rs
+++ crates/space_assets/src/gltf_loader.rs
@@ -1,6 +1,8 @@
 use std::io::Read;
 use std::path::PathBuf;
 use std::sync::{Arc, Mutex};
+use bevy::ecs::world::EntityMut;
+use bevy::prelude::Entity;
 use byteorder::ByteOrder;
 use gltf::animation::Property::Rotation;
 use gltf::json::accessor::ComponentType;
@@ -15,7 +17,8 @@ use crate::mesh::{GMesh, GVertex, Material, TextureBundle};
 pub trait GltfAssetLoader {
     fn load_gltf_color_texture(&mut self, base : &String, src : Option<gltf::texture::Info>, gamma : bool) -> Handle<TextureBundle>;
     fn load_gltf_normal_texture(&mut self, base : &String, src : Option<gltf::material::NormalTexture>) -> Handle<TextureBundle>;
-    fn wgpu_gltf_load(&mut self, device : &wgpu::Device, path : String, world : &mut space_core::bevy::prelude::World);
+    fn wgpu_gltf_load(&mut self, device : &wgpu::Device, path : String, world : &mut space_core::bevy::prelude::World) -> Vec<Entity>;
+    fn wgpu_gltf_load_cmds(&mut self, device : &wgpu::Device, path : String, commands : &mut space_core::ecs::Commands) -> Vec<Entity>;
 }
 
 impl GltfAssetLoader for SpaceAssetServer {
@@ -47,8 +50,191 @@ impl GltfAssetLoader for SpaceAssetServer {
     }
 
 
+    fn wgpu_gltf_load_cmds(&mut self, device : &wgpu::Device, path : String, commands : &mut space_core::ecs::Commands) -> Vec<Entity> {
+        let mut res = vec![];
 
-    fn wgpu_gltf_load(&mut self, device : &wgpu::Device, path : String, world : &mut space_core::bevy::prelude::World) {
+        let sponza = gltf::Gltf::open(&path).unwrap();
+        let base = PathBuf::from(&path).parent().unwrap().to_str().unwrap().to_string();
+
+        let mut buffers = vec![];
+        for buf in sponza.buffers() {
+            match buf.source() {
+                gltf::buffer::Source::Bin => {
+
+                }
+                gltf::buffer::Source::Uri(uri) => {
+
+                    let mut f = std::fs::File::open(format!("{}/{}", &base, uri)).unwrap();
+                    let metadata = std::fs::metadata(&format!("{}/{}", &base, uri)).unwrap();
+                    let mut byte_buffer = vec![0; metadata.len() as usize];
+                    f.read(&mut byte_buffer).unwrap();
+                    buffers.push(byte_buffer);
+                }
+            }
+        }
+
+        let mut meshes = vec![];
+
+        for m in sponza.meshes() {
+            let mut combined = vec![];
+            for p in m.primitives() {
+                let mut pos : Vec<f32> = vec![];
+                let mut normals : Vec<f32> = vec![];
+                let mut uv : Vec<f32> = vec![];
+                let mut tangent : Vec<f32> = vec![];
+
+                let indices_acc = p.indices().unwrap();
+                let indices_view = indices_acc.view().unwrap();
+                let mut indices;
+
+                match indices_acc.data_type() {
+                    ComponentType::U16 => {
+                        indices = vec![0; indices_acc.count()];
+                        let buf = &buffers[indices_view.buffer().index()];
+                        for idx in 0..indices.len() {
+                            let global_idx = idx * indices_view.stride().unwrap_or(2) + indices_view.offset() + indices_acc.offset();
+                            indices[idx] = byteorder::LittleEndian::read_u16(&buf[global_idx..(global_idx + 2)]) as u32;
+                        }
+                    }
+                    ComponentType::U32 => {
+                        indices = vec![0; indices_acc.count()];
+                        let buf = &buffers[indices_view.buffer().index()];
+                        for idx in 0..indices.len() {
+                            let global_idx = idx * indices_view.stride().unwrap_or(4) + indices_view.offset() + indices_acc.offset();
+                            indices[idx] = byteorder::LittleEndian::read_u32(&buf[global_idx..(global_idx + 4)]) as u32;
+                        }
+                    }
+                    _ => {panic!("Unsupported index type!");}
+                }
+
+                for (sem, acc) in p.attributes() {
+                    // match  { }
+                    let view = acc.view().unwrap();
+                    let mut data = vec![0.0f32; acc.count() * acc.dimensions().multiplicity()];
+
+                    let stride = view.stride().unwrap_or(acc.data_type().size() * acc.dimensions().multiplicity());
+
+                    let buf = &buffers[view.buffer().index()];
+
+                    for c in 0..acc.count() {
+                        for d in 0..acc.dimensions().multiplicity() {
+                            let idx = c * acc.dimensions().multiplicity() + d;
+                            let global_idx = c * stride + acc.offset() + view.offset() + d * acc.data_type().size();
+                            data[idx] = byteorder::LittleEndian::read_f32(&buf[global_idx..(global_idx + 4)]);
+                        }
+                    }
+
+                    match sem {
+                        Semantic::Positions => {
+                            pos.extend(data.iter());
+                        }
+                        Semantic::Normals => {
+                            normals.extend(data.iter());
+                        }
+                        Semantic::Tangents => {
+                            tangent.extend(data.iter());
+                        }
+                        Semantic::Colors(_) => {}
+                        Semantic::TexCoords(_) => {
+                            uv.extend(data.iter());
+                        }
+                        Semantic::Joints(_) => {}
+                        Semantic::Weights(_) => {}
+                        _ => {}
+                    }
+                }
+                //load diffuse texture
+                let vertex_count = pos.len() / 3;
+                let triangle_count = indices.len() / 3;
+
+                if uv.len() == 0 {
+                    uv = vec![0.0f32; pos.len() / 3 * 2];
+                }
+
+                if tangent.len() == 0 {
+                    tangent = vec![0.0f32; pos.len()];
+                    println!("[ERROR] No tangents for object!");
+                }
+
+
+
+                let mut verts = vec![];
+                for i in 0..vertex_count {
+                    let shift = i * 3;
+                    let uv_shift = i * 2;
+                    verts.push( GVertex {
+                        pos: [pos[shift], pos[shift + 1], pos[shift + 2]],
+                        normal: [normals[shift], normals[shift + 1], normals[shift + 2]],
+                        tangent: [tangent[shift], tangent[shift + 1], tangent[shift + 2]],
+                        uv: [uv[uv_shift], uv[uv_shift + 1]],
+                    });
+                }
+
+                let vert_buffer = device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
+                    label: Some("gltf vertex buffer"),
+                    contents: bytemuck::cast_slice(&verts),
+                    usage: wgpu::BufferUsages::VERTEX
+                });
+
+                let index_buf = device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
+                    label: Some("gltf index buffer"),
+                    contents: bytemuck::cast_slice(&indices),
+                    usage: wgpu::BufferUsages::INDEX
+                });
+
+                let model = GMesh {
+                    vertex : vert_buffer,
+                    index : index_buf,
+                    index_count : indices.len() as u32
+                };
+
+                let normal = self.load_gltf_normal_texture(&base, p.material().normal_texture());
+
+                let color = self.load_gltf_color_texture(&base, p.material().pbr_metallic_roughness().base_color_texture(), true);
+
+                let mr = self.load_gltf_color_texture(&base, p.material().pbr_metallic_roughness().metallic_roughness_texture(), true);
+
+                let material = Material {
+                    color,
+                    normal,
+                    metallic_roughness: mr,
+                    gbuffer_bind : None,
+                    version_sum : 0
+                };
+
+                combined.push((GMeshPtr {mesh : Arc::new(model)}, material));
+            }
+            meshes.push(combined);
+        }
+
+        for n in sponza.nodes() {
+            if let Some(mesh_idx) = n.mesh() {
+                let mut location = Location::new(&device);
+
+                let (tr, quat, scale) = n.transform().decomposed();
+                let q = nalgebra::Quaternion::new(quat[3], quat[0], quat[1], quat[2]);
+                let rot = nalgebra::Rotation::from(nalgebra::UnitQuaternion::from_quaternion(q));
+                let (e_x, e_y, e_z) = rot.euler_angles();
+                location.pos = tr.into();
+                location.rotation = [e_x, e_y, e_z].into();
+                location.scale = scale.into();
+
+                for (p, m) in &meshes[mesh_idx.index()] {
+                    res.push(
+                        commands.spawn(MeshBundle {
+                        mesh : p.clone(),
+                        location : location.clone(&device),
+                        material : m.clone()
+                    }).id());
+                }
+            }
+        }
+
+        res
+    }
+
+    fn wgpu_gltf_load(&mut self, device : &wgpu::Device, path : String, world : &mut space_core::bevy::prelude::World) -> Vec<Entity> {
+        let mut res = vec![];
 
         let sponza = gltf::Gltf::open(&path).unwrap();
         let base = PathBuf::from(&path).parent().unwrap().to_str().unwrap().to_string();
@@ -218,14 +404,17 @@ impl GltfAssetLoader for SpaceAssetServer {
 
                 for (p, m) in &meshes[mesh_idx.index()] {
 
-                    world.spawn(MeshBundle {
+                    let e = world.spawn(MeshBundle {
                         mesh : p.clone(),
                         location : location.clone(&device),
                         material : m.clone()
                     });
+                    res.push(e.id());
                 }
             }
         }
+
+        res
     }
 }
 
--- src/scenes/station_build_scene.rs
+++ src/scenes/station_build_scene.rs
@@ -1,46 +1,65 @@
 use std::default::default;
 use bevy::asset::AssetServer;
 use egui::{Context, Ui};
-use space_game::{Game, GameCommands, SchedulePlugin, GlobalStageStep, EguiContext, SceneType, RonAssetPlugin};
+use space_game::{Game, GameCommands, SchedulePlugin, GlobalStageStep, EguiContext, SceneType, RonAssetPlugin, RenderApi};
 use space_render::add_game_render_plugins;
 use space_core::{ecs::*, app::App};
 use space_core::serde::*;
 use bevy::reflect::*;
 use bevy::asset::*;
+use space_assets::{GltfAssetLoader, SpaceAssetServer};
 
 #[derive(Default, Deserialize, TypeUuid, Debug, Clone)]
 #[uuid = "fce6d1f5-4317-4077-b23e-6099747b08dd"]
-struct BlockDesc {
+struct RonBlockDesc {
     pub name : String,
     pub model_path : String
 }
 
 
+
 #[derive(Resource, Default)]
 struct StationBlocks {
-    pub panels : Vec<Handle<BlockDesc>>,
+    pub panels : Vec<Handle<RonBlockDesc>>,
 
-    pub active_block : Option<BlockDesc>
+    pub active_block : Option<RonBlockDesc>,
+    pub active_entity : Option<Entity>
+
 }
 
 
 fn station_menu(
+    mut commands : Commands,
     ctx : Res<EguiContext>,
     mut panels : ResMut<StationBlocks>,
-    blocks : Res<Assets<BlockDesc>>
+    blocks : Res<Assets<RonBlockDesc>>,
+    mut asset_server : ResMut<SpaceAssetServer>,
+    render : Res<RenderApi>
 ) {
     egui::SidePanel::left("Build panel").show(&ctx, |ui| {
-        egui::Grid::new("Floor block grid").show(ui, |ui| {
-            ui.label("Blocks:");
-            ui.end_row();
-            for h in &panels.panels {
-                if let Some(block) = blocks.get(h) {
-                    if ui.button(&block.name).clicked() {
-                        panels.active_block = Some(block.clone());
-                    }
+        if let Some(block) = panels.active_block.as_ref() {
+            ui.label(format!("Selected block: {}", block.name));
+        } else {
+            ui.label(format!("Selected block: None"));
+        }
+        ui.separator();
+
+
+        ui.label("Blocks:");
+        let mut panel_list = panels.panels.clone();
+        for h in &panel_list {
+            if let Some(block) = blocks.get(h) {
+                if ui.button(&block.name).clicked() {
+                    panels.active_block = Some(block.clone());
+
+                    asset_server.wgpu_gltf_load_cmds(
+                        &render.device,
+                        block.model_path.clone(),
+                        &mut commands
+                    );
                 }
             }
-        });
+        }
     });
 }
 
@@ -63,7 +82,7 @@ impl SchedulePlugin for StationBuildMenu {
 
     fn add_system(&self, app : &mut App) {
 
-        app.add_plugin(RonAssetPlugin::<BlockDesc>{ ext: vec!["wall"], ..default() });
+        app.add_plugin(RonAssetPlugin::<RonBlockDesc>{ ext: vec!["wall"], ..default() });
 
         app.add_system_set(SystemSet::on_enter(SceneType::StationBuilding)
             .with_system(init_station_build));
