85dce8f6bfee6f7d5f1087a3f825791443452e36
Andrey Yamaev <a.yamaev@smartengines.com>
2022-11-17 16:42:10 +0300

    add: build on lattice

--- src/scenes/station_build_scene.rs
+++ src/scenes/station_build_scene.rs
@@ -4,33 +4,83 @@ use bevy::prelude::{info_span, info};
 use egui::{Context, Ui};
 use space_game::{Game, GameCommands, SchedulePlugin, GlobalStageStep, EguiContext, SceneType, RonAssetPlugin, RenderApi, InputSystem, KeyCode, ScreenSize};
 use space_render::{add_game_render_plugins, AutoInstancing};
-use space_core::{ecs::*, app::App, nalgebra};
+use space_core::{ecs::*, app::App, nalgebra, SpaceResult};
 use space_core::{serde::*, Camera};
-use bevy::reflect::*;
 use bevy::asset::*;
 use bevy::utils::HashMap;
 use winit::event::MouseButton;
 use space_assets::{GltfAssetLoader, Location, Material, MeshBundle, SpaceAssetServer, GMesh, LocationInstancing, SubLocation};
+use bevy::reflect::TypeUuid;
+use std::string::String;
 
 #[derive(Component)]
 struct StationBuildActiveBlock {}
 
 pub struct StationBuildMenu {}
 
+#[derive(Default)]
 pub struct BlockID(usize);
 
-#[derive(Component)]
+#[derive(Resource)]
 pub struct StationChunk {
-    pub origin : nalgebra::Vector3<i32>,
-    pub chunk_size : f32,
+    pub origin : nalgebra::Point3<i32>,
+    pub voxel_size : f32,
     pub floors : Vec<BlockID>,
-
+    pub chunk_size : nalgebra::Vector3<i32>,
     pub instance_renders : HashMap<BlockID, Entity>
 }
 
-#[derive(Component)]
-pub struct Station {
+impl Default for StationChunk {
+    fn default() -> Self {
+        Self {
+            origin : nalgebra::Point3::default(),
+            voxel_size : 2.0,
+            floors : vec![],
+            chunk_size : nalgebra::Vector3::new(16, 16, 16),
+            instance_renders : HashMap::new()
+        }
+    }
+}
 
+impl StationChunk {
+    pub fn add_block(
+        &mut self,
+        pos : nalgebra::Vector3<f32>,
+        id : BlockID,
+        mesh : &Handle<GMesh>,
+        mat : &Handle<Material>) -> SpaceResult<()> {
+
+
+        Ok(())
+    }
+
+    pub fn get_idx_3d(&self, pos : &nalgebra::Point3<f32>) -> nalgebra::Point3<i32> {
+        let origin_world = nalgebra::Vector3::<f32>::new(
+            self.origin.x as f32,
+            self.origin.y as f32,
+            self.origin.z as f32
+        ) * self.voxel_size;
+
+        let dp = pos - origin_world;
+        if dp.x < 0.0 || dp.y < 0.0 || dp.z < 0.0 {
+
+        }
+        let x = (dp.x / self.voxel_size).round() as i32;
+        let y = (dp.y / self.voxel_size).round() as i32;
+        let z = (dp.z / self.voxel_size).round() as i32;
+
+        nalgebra::Point3::new(x, y, z)
+    }
+
+    pub fn get_chunked_pos(&self, pos : &nalgebra::Point3<f32>) -> nalgebra::Point3<f32> {
+        let idxs = self.get_idx_3d(pos);
+
+        nalgebra::Point3::new(
+            idxs.x as f32 * self.voxel_size,
+            idxs.y as f32 * self.voxel_size,
+            idxs.z as f32 * self.voxel_size,
+        )
+    }
 }
 
 impl SchedulePlugin for StationBuildMenu {
@@ -60,7 +110,8 @@ fn add_block_to_station(
     world : &World,
     input : Res<InputSystem>,
     mut panels : Res<StationBlocks>,
-    render : Res<RenderApi>) {
+    render : Res<RenderApi>,
+    chunk : Res<StationChunk>) {
 
     if input.get_mouse_button_state(&MouseButton::Left) {
         if let Some(e) = panels.active_entity.as_ref() {
@@ -82,6 +133,7 @@ fn place_block(
     input : Res<InputSystem>,
     mut panels : ResMut<StationBlocks>,
     screen_size : Res<ScreenSize>,
+    chunk : Res<StationChunk>,
     render : Res<RenderApi>) {
 
     let ray = camera.screen_pos_to_ray(
@@ -89,7 +141,8 @@ fn place_block(
         nalgebra::Point2::<f32>::new(screen_size.size.width as f32, screen_size.size.height as f32));
 
     for mut loc in query.iter_mut() {
-        let point = ray.interact_y(panels.build_level as f32);
+        let ray_point = ray.interact_y(panels.build_level as f32);
+        let point = chunk.get_chunked_pos(&ray_point);
         // let point = ray.pos + 10.0 * ray.dir;
 
         loc.pos.x = point.x;
@@ -129,6 +182,7 @@ struct StationBlocks {
     pub panels : Vec<Handle<RonBlockDesc>>,
 
     pub active_block : Option<RonBlockDesc>,
+    pub active_id : BlockID,
     pub active_entity : Option<Entity>,
     pub build_level : i32,
 }
@@ -142,7 +196,8 @@ fn station_menu(
     mut asset_server : ResMut<SpaceAssetServer>,
     render : Res<RenderApi>,
     mut materials : ResMut<Assets<Material>>,
-    mut meshes : ResMut<Assets<GMesh>>
+    mut meshes : ResMut<Assets<GMesh>>,
+    mut chunk : ResMut<StationChunk>
 ) {
     egui::SidePanel::left("Build panel").show(&ctx, |ui| {
         if let Some(block) = panels.active_block.as_ref() {
@@ -155,12 +210,10 @@ fn station_menu(
 
         ui.label("Blocks:");
         let mut panel_list = panels.panels.clone();
-        for h in &panel_list {
+        for (idx, h) in panel_list.iter().enumerate() {
             if let Some(block) = blocks.get(h) {
                 if ui.button(&block.name).clicked() {
                     panels.active_block = Some(block.clone());
-
-
                     let bundles = asset_server.wgpu_gltf_load_cmds(
                         &render.device,
                         block.model_path.clone(),
@@ -172,7 +225,7 @@ fn station_menu(
                             .insert(StationBuildActiveBlock{}).id();
                         panels.active_entity = Some(e);
                     }
-
+                    panels.active_id = BlockID(idx);
                 }
             }
         }
@@ -230,5 +283,7 @@ fn init_station_build(
     camera.frw = camera.frw.normalize();
 
     camera.up =  camera.get_right().cross(&camera.frw).normalize();
+
+    commands.insert_resource(StationChunk::default());
 }
 
